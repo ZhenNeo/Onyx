883ff751eaccf32a7d1a561a7b6948b7a8fcc934
ZhenNeo <abhi21vk@gmail.com>
2026-03-20 12:06:23 +0530

    added a simple notification system, am running into some errors regarding that

--- app/src/main/java/com/example/onyx/MainActivity.kt
+++ app/src/main/java/com/example/onyx/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.onyx
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,25 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.onyx.ui.theme.OnyxTheme
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Button
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        createNotificationChannel(this)
         setContent {
             OnyxTheme {
                 Surface(
@@ -39,6 +40,37 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+fun createNotificationChannel(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channel = NotificationChannel(
+            "Drift Channel",
+            "Drift Check",
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            description = "Check if you are drifting"
+        }
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
+    }
+}
+
+
+
+fun showNotification(context: Context) {
+    val builder = NotificationCompat.Builder(context, "drift_channel")
+        .setSmallIcon(android.R.drawable.ic_dialog_info)
+        .setContentTitle("Check-in")
+        .setContentText("Are you drifting?")
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+    with(NotificationManagerCompat.from(context)) {
+        notify(System.currentTimeMillis().toInt(), builder.build())
+    }
+}
+
 
 
 @Composable
